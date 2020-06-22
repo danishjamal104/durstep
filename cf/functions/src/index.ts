975069f8e21c6cfdec0254ee81bd0a2782ea631a
Danish Jamal <danishjamal.104@gmail.com>
2020-06-22 16:28:36 +0530

    Notify cf done

---
+++ cf/functions/src/index.ts
@@ -0,0 +1,44 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+//import * as nodemailer from 'nodemailer';
+//import * as cors from 'cors';
+admin.initializeApp()
+
+export const notify = 
+functions.https.onRequest((req, res)=>{
+    
+    //https://us-central1-durstep-7e7a8.cloudfunctions.net/notify?to=gre4OYbgiEZb5StRpOAldyVoZwD2&title=HttpText&msg=Hello
+
+    const to = ""+req.query.to
+    const title= ""+req.query.title
+    const message = ""+req.query.msg
+
+    console.log(`to lenght=${to?.length}`)
+    console.log(title)
+    console.log(message)
+
+
+    admin.firestore().doc(`user/${to}`).get()
+    .then(ud => {
+        const push_token = ud.data()?.push_token
+
+        if(push_token===null){
+            return res.send("No token found");
+        }
+
+        const payload = {
+            token: push_token,
+            notification: {
+                  title: title,
+                  body: message
+                }
+            }
+        return admin.messaging().send(payload)
+        .then(()=>{
+            return res.send('Send')
+        })
+        .catch((error) => {return res.send(error)})
+
+    }).catch((error) => {return res.send(error)})
+
+})
