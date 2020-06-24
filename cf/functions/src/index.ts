dcf5dbb82a5c02b8902b3f4d3bf4c088f8a12038
Danish Jamal <danishjamal.104@gmail.com>
2020-06-24 23:37:13 +0530

    Delivery done, notify remaining

--- cf/functions/src/index.ts
+++ cf/functions/src/index.ts
@@ -9,12 +9,17 @@ functions.https.onRequest((req, res)=>{
     //https://us-central1-durstep-7e7a8.cloudfunctions.net/notify?to=gre4OYbgiEZb5StRpOAldyVoZwD2&title=HttpText&msg=Hello
     const to = ""+req.query.to
     const title= ""+req.query.title
-    const message = ""+req.query.msg
+    let message = ""+req.query.msg
+    const type = ""+req.query.type
+
     admin.firestore().doc(`user/${to}`).get()
     .then(ud => {
         const push_token = ud.data()?.push_token
         if(push_token===null){
             return res.send("No token found");
+        }
+        if(type==="1"){
+            message += `${ud.data()?.name}, ${ud.data()?.number}`
         }
         const payload = {
             token: push_token,
@@ -90,28 +95,83 @@ functions.firestore.document('active_delivery/{distributorId}')
         .catch(error => console.log(error))
     }).catch(error => console.log(error))
 })
-export const exceptNewDelivery = 
-functions.firestore.document('delivery/{dId}')
-.onCreate(dt => {
-    const delivery = dt.data()
-    const order = {
-        to: delivery?.to,
-        from: admin.firestore().doc(`user/${delivery?.active.id}`),
-        amount: delivery?.amount,
-        time: delivery?.time
-    }
-    return admin.firestore().collection(`orders/${dt.id}`).add(order)
-    .then(() => {
-        const promises:any = []
-        promises.push(delivery?.subRef.update('active', null))
-        promises.push(admin.firestore().doc(`delivery/${dt.id}`).delete())
-        return Promise.all(promises)
-        .then(() => {
-            return delivery?.active.update({
-                completed: admin.firestore.FieldValue.increment(1),
-                pending: admin.firestore.FieldValue.increment(-1),
-                delivered_list: admin.firestore.FieldValue.arrayUnion(delivery?.subRef)
-            })
-        }).catch(error => console.log(error))
-    }).catch(error => console.log(error))
+export const scan = 
+functions.https.onRequest((req, res) => {
+//https://us-central1-durstep-7e7a8.cloudfunctions.net/scan?to=
+
+    //https:<domain>.com/scan?to={uId}&l={litreAmount}
+    const to = req.query.to
+    const amountSt = req.query.l
+    const amount: number = parseFloat(<string>amountSt)
+
+
+    admin.firestore().collection(`user/${to}/subscriptions/`).orderBy('sDate', 'desc').get()
+    .then(querySnap => {
+        // complete function by getting data
+        if(querySnap.empty){
+            return res.send('No Subscription!!')
+        }
+        const docs = querySnap.docs
+        let active_sub
+        let isAnyActive = false
+        for (let i = 0; i < docs.length; i++) {
+            const element = docs[i];
+            if(element.data().active!=null){
+                isAnyActive = true
+                active_sub = element.data()
+                break
+            }
+        }
+        if(!isAnyActive){
+            return res.send('No Active Delivery!!')
+        }
+        return (<admin.firestore.DocumentReference> active_sub?.active).get()
+        .then(adt => {
+            //adt = active_delivery 
+            const dt = adt.data()
+            const subscription_list: admin.firestore.DocumentReference[] = 
+            dt?.subscription_list
+            const order = {
+                to: to,
+                from: adt.id,
+                amount: amount,
+                time: admin.firestore.Timestamp.now(),
+                feedback: null,
+            }
+            return admin.firestore().collection('orders').add(order)
+            .then(()=>{
+                // till here order is created
+                // next step is to update active_delivery
+                const subs_ref: admin.firestore.DocumentReference = 
+                admin.firestore().doc(`user/${to}/subscriptions/${active_sub?.sId}`)
+                const idx = subscription_list.indexOf(subs_ref)
+                delete subscription_list[idx]
+                const delivered_list: Array<admin.firestore.DocumentReference> = 
+                dt?.delivered_list
+                const newDt = {
+                    subscription_list: subscription_list,
+                    delivered_list: delivered_list.push(subs_ref),
+                    total: dt?.total,
+                    pending: dt?.pending-1,
+                    delivered: dt?.delivered+1,
+                    location: dt?.location
+                }
+                return adt.ref.update(newDt)
+                .then(()=>{
+                    return admin.firestore().doc(`user/${to}/subscriptions/${active_sub?.sId}`).update('active', null)
+                    .then(()=>{
+                        const title = 'Success'
+                        const msg = 'Order succesfully delivered by '
+                        const msgUrl = `https://us-central1-durstep-7e7a8.cloudfunctions.net/notify?to=${adt.id}&title=${title}&msg=${msg}&type=1`
+                        return res.send({
+                            status: 200,
+                            msg: 'Order Delivered',
+                            notify: msgUrl
+                        })
+                    }).catch((err)=>{return res.send('Error setting active to null'+err)})
+                }).catch((err) => {return res.send('Error Updating Active Delivery!!'+err)})
+            }) .catch((err) => {return res.send('Error Creating Order!!'+err)})
+        }).catch(() => {return res.send('Delivery Not Found!!')})
+    }).catch(() => {return res.send('Invalid User!!')})
+
 })
